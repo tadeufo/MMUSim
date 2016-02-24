88267620aa637ad8ef874429c7bce7ac26a11ac9
Tadeu Ferreira Oliveira <tadeu.fo@gmail.com>
2016-02-24 10:06:18 -0300

    Classe que define armazenamento de memória de um processo

--- src/mmusim/Processo.java
+++ src/mmusim/Processo.java
@@ -6,16 +6,33 @@
 package mmusim;
 
 /**
- *
+ * Mantem os dados de um processo
  * @author tadeu
  */
-public class Processo {
+public class Processo {   
     private int enderecoBase;
     private int enderecoLimite;
+    /**
+     * emMemoria indica se o processo esta na memoria principal(true) ou na 
+     * memoria secundaria(false)
+     */
+    private boolean emMemoria;
+    /**
+     * Caso o processo esteja em swap (emMemoria==false) indica o endereco inicial
+     * que os dados dos processo estao no disco
+     */
+    private int enderecoSwap;
 
+    /**
+     * Cria um novo processo carregado na memoria
+     * @param enderecoBase endereço inicial onde o processo foi carregado
+     * @param enderecoLimite endereço maximo acessivel pelo processo
+     */
     public Processo(int enderecoBase, int enderecoLimite) {
         this.enderecoBase = enderecoBase;
         this.enderecoLimite = enderecoLimite;
+        this.emMemoria = true;
+        this.enderecoSwap = 0;
     }
 
     public int getEnderecoBase() {
